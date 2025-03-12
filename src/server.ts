720ac3ec8f09531074c2808f350c1985d75acda5
Raquelll77 <raquelalvarado1717@gmail.com>
2025-03-12 11:49:00 -0600

    agregando rutas y llamandolas con POSTMAN

--- src/server.ts
+++ src/server.ts
@@ -2,16 +2,25 @@ import express from 'express';
 
 const server = express();
 
-
 //Routing
 server.get('/', (req, res)=> {
+    res.json('desde GET')
+})
 
-    const datos = [
-        {id: 1, nombre: 'Juan'},
-        {id:3, nombre:'Raquel'}
-    ]
-    
-    res.send(datos)
+server.post('/', (req, res)=> {
+    res.json('desde POST')
+})
+
+server.put('/', (req, res)=> {
+    res.json('desde PUT')
+})
+
+server.patch('/', (req, res)=> {
+    res.json('desde PATCH')
+})
+
+server.delete('/', (req, res)=> {
+    res.json('desde DELETE')
 })
 
 export default server
