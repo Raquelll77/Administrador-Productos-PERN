bac961b5274d669f9e2b8ce6a13b00b9e71fb6eb
Raquelll77 <raquelalvarado1717@gmail.com>
2025-12-11 10:51:11 -0600

    leyendo de otra manera los datos del formulario y guardando en bd usando propiedad de los modelos

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -3,11 +3,15 @@ import Product from "../models/Product.model"
 
 //siempre que interactuamos con los modelos las funciones tienen que ser asincronas detener el codigo hasta obtener resultados
 export const createProduct = async (req : Request, res : Response)=> {
+    // otra manera de hacerlo creando el objeto
+    // const product = new Product(req.body)
 
-    const product = new Product(req.body)
+    // //almacenar en la base de datos
+    // const savedProduct = await product.save()
+    // res.json({data: savedProduct})
 
-    //almacenar en la base de datos
-    const savedProduct = await product.save()
+    //manera directa de hacerlo usando las propiedades de los modelos
+    const product = await Product.create(req.body)
     
-    res.json({data: savedProduct})
+    res.json({data: product})
 }
